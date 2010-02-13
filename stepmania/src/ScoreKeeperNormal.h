6a92dbff334b50a4e8281ef5d8fdf7b8e8a09aa7
glenn <glenn@822b03c2-8f45-0410-a444-95660a741a6f>
2010-02-13 06:54:46 +0000

    working on routine
    git-svn-id: https://svn.stepmania.com/svn/trunk@28287 822b03c2-8f45-0410-a444-95660a741a6f

--- stepmania/src/ScoreKeeperNormal.h
+++ stepmania/src/ScoreKeeperNormal.h
@@ -62,7 +62,6 @@ class ScoreKeeperNormal: public ScoreKeeper
 	virtual void AddTapScore( TapNoteScore tns );
 	virtual void AddHoldScore( HoldNoteScore hns );
 	virtual void AddTapRowScore( TapNoteScore tns, const NoteData &nd, int iRow );
-	virtual void HandleTapScoreNone();
 
 	/* Configuration: */
 	/* Score after each tap will be rounded to the nearest m_iRoundTo; 1 to do nothing. */
@@ -85,6 +84,7 @@ public:
 	void HandleHoldScore( const TapNote &tn );
 	void HandleHoldActiveSeconds( float fMusicSecondsHeld ) {};
 	void HandleHoldCheckpointScore( const NoteData &nd, int iRow, int iNumHoldsHeldThisRow, int iNumHoldsMissedThisRow );
+	void HandleTapScoreNone();
 
 	// This must be calculated using only cached radar values so that we can 
 	// do it quickly.
