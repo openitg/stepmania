b6c1f2a667f22d423ebe2ebd6ec1ed529c42ceb3
shakesoda <shakesoda@822b03c2-8f45-0410-a444-95660a741a6f>
2010-03-02 08:46:10 +0000

    Hack to fix the stuttering that occurs on some videos (courtesy of Henke37).
    git-svn-id: https://svn.stepmania.com/svn/trunk@28324 822b03c2-8f45-0410-a444-95660a741a6f

--- stepmania/src/arch/MovieTexture/MovieTexture_FFMpeg.cpp
+++ stepmania/src/arch/MovieTexture/MovieTexture_FFMpeg.cpp
@@ -284,6 +284,8 @@ private:
 	avcodec::AVPacket m_Packet;
 	int m_iCurrentPacketOffset;
 
+	float m_fLastFrame;
+
 	/* 0 = no EOF
 	 * 1 = EOF from ReadPacket
 	 * 2 = EOF from ReadPacket and DecodePacket */
@@ -301,6 +303,8 @@ MovieDecoder_FFMpeg::MovieDecoder_FFMpeg()
 	/* Until we play the whole movie once without hitting a B-frame, assume
 	 * they exist. */
 	m_bHadBframes = true;
+
+	m_fLastFrame = 0;
 
 	Init();
 }
@@ -334,6 +338,16 @@ void MovieDecoder_FFMpeg::Init()
 /* Read until we get a frame, EOF or error.  Return -1 on error, 0 on EOF, 1 if we have a frame. */
 int MovieDecoder_FFMpeg::DecodeFrame( float fTargetTime )
 {
+	// xxx: hack to filter out stuttering
+	if(fTargetTime<m_fLastFrame)
+	{
+		fTargetTime=m_fLastFrame;
+	}
+	else
+	{
+		m_fLastFrame=fTargetTime;
+	}
+
 	while( 1 )
 	{
 		int ret = DecodePacket( fTargetTime );
