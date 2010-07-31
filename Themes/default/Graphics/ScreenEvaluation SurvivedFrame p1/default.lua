44426ea95e80e9a030f25b5940256e05c7936b8a
shakesoda <shakesoda@822b03c2-8f45-0410-a444-95660a741a6f>
2010-07-31 20:39:16 +0000

    Remove all themes (if you'd like something usable, take the default from trunk until this is running again).
    git-svn-id: https://svn.stepmania.com/svn/branches/exp@28404 822b03c2-8f45-0410-a444-95660a741a6f

--- Themes/default/Graphics/ScreenEvaluation SurvivedFrame p1/default.lua
+++
@@ -1,6 +0,0 @@
-return Def.ActorFrame{
-	LoadActor("frame");
-	LoadFont("_terminator two 13px")..{
-		InitCommand=cmd(y,-46;shadowlength,0;strokecolor,color("#333333FF");settext,ScreenString("SongsSurvived"););
-	};
-};
