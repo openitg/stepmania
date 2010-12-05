6584388cb7245762639c14e5c5b2e97afbfac49d
shakesoda <shakesoda@822b03c2-8f45-0410-a444-95660a741a6f>
2010-12-05 06:22:15 +0000

    Use the release script from trunk.
    git-svn-id: https://svn.stepmania.com/svn/branches/4.0@28550 822b03c2-8f45-0410-a444-95660a741a6f

--- stepmania/PBProject/scripts/mkrelease.pl
+++ stepmania/PBProject/scripts/mkrelease.pl
@@ -7,7 +7,9 @@ use File::Basename;
 use File::Temp qw/tempfile tempdir/;
 use Cwd;
 
-my @docs = ( "Licenses.txt" );
+my @docs = (
+	"Licenses.txt"
+);
 
 # Passing a date for a CVS release gives StepMania-CVS-date.
 # Otherwise you get StepMania-ver.
@@ -30,7 +32,6 @@ while( <FH> )
 	} elsif( /^#define\s+PRODUCT_VER_BARE\s+(.*?)\s*$/ ) {
 		$ver = $1;
 	}
-	
 }
 close FH;
 
@@ -39,57 +40,18 @@ my $destname = @ARGV ? "$id-$ARGV[0]" : "$family-$ver";
 $destname =~ s/\s+/-/g;
 
 my $tmp = tempdir;
-my $dstdir = "$tmp/$id";
-mkdir $dstdir;
 
-# Copy StepMania
-system 'cp', '-r', "$srcdir/StepMania.app", $dstdir and die "cp -r failed: $!\n";
-system 'strip', '-x', "$dstdir/StepMania.app/Contents/MacOS/StepMania";
+# Copy StepMania and make smzip
+system 'cp', '-r', "$srcdir/StepMania.app", $tmp and die "cp -r failed: $!\n";
+system 'strip', '-x', "$tmp/StepMania.app/Contents/MacOS/StepMania";
 
-# SUPER HACK: Setting the position of the first non-hidden file icon doesn't take effect for some reason.  Figure out why eventually so that we can remove 0dummy.
-system 'cp', 'scripts/0dummy', "$tmp/" and die "ln -s failed: $!\n";
-system 'cp', 'scripts/Applications', "$tmp/" and die "ln -s failed: $!\n";
+mkdir "$tmp/Packages";
+system "$srcdir/Utils/CreatePackage.pl", $srcdir, "$tmp/Packages/" and die "mksmdata.pl failed: $!\n";
 
-#system "$srcdir/Utils/CreatePackage.pl", $srcdir, "$dstdir/StepMania.app/Contents/Resources" and die "mksmdata.pl failed: $!\n";
+# Copy docs
+mkdir "$tmp/Docs";
+copy "$srcdir/Docs/$_", "$tmp/Docs/$_" for @docs;
 
-my @files = (
-	"Announcers",
-	"BackgroundEffects",
-	"BackgroundTransitions",
-	"BGAnimations",
-	"CDTitles",
-	"Characters",
-	"Courses/Samples",
-	"Data",
-	"NoteSkins/common/default",
-	"NoteSkins/dance/default",
-	"NoteSkins/dance/flat",
-	"Packages",
-	"RandomMovies",
-	"Songs",
-	"Themes/default",
-);
-foreach my $x ( @files )
-{
-	print "$dstdir/" . dirname $x . "\n";
-	mkpath "$dstdir/" . dirname $x;
-	system 'cp', '-r', "$srcdir/$x", "$dstdir/$x" and die "cp -r failed: $!\n";
-}
-
-
-# clean up .svn dirs
-my @svndirs = split /\n/, `find "$dstdir" -type d -name .svn`;
-rmtree \@svndirs;
-
-
-# Make a dmg (old)
-#system qw/hdiutil create -ov -format UDZO -imagekey zlib-level=9 -srcfolder/, $tmp, '-volname', $destname, "$root/$destname-mac.dmg";
-my $dmg = "$root/$destname-mac.dmg";
-
-# Make a dmg (new)
-system 'rm', $dmg;
-system 'sh', 'scripts/yoursway-create-dmg-1.0.0.2/create-dmg', '--window-size', '750', '370', '--background', 'scripts/background.png', '--icon-size', '64', '--volname', "$family $ver", '--icon', '0dummy', '600', '250', '--icon', 'Applications', '450', '250', '--icon', $id, '270', '250', $dmg, $tmp;
-
-
+# Make a dmg
+system qw/hdiutil create -ov -format UDZO -imagekey zlib-level=9 -srcfolder/, $tmp, '-volname', $destname, "$root/$destname-mac.dmg";
 rmtree $tmp;
-
